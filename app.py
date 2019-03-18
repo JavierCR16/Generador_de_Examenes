b5f9f1b74f92313f1b43cf5afa7c99d8228e948c
Randall2910 <ramoya@estudiantec.cr>
2019-03-18 13:04:13 -0600

    Agregé un botón para que cuando lo presione abrá el encabezado

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World!' + "<html><body><button type= button>Click Me</button></body></html>"
 
 
 if __name__ == '__main__':
